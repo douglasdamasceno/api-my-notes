9b7d7e405aacd1128947f8f35bdb1f3f1fbe6a26
douglasdm <douglasmdamasceno@gmail.com>
2020-04-14 11:58:58 -0300

    first commit

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect("mongodb://localhost:javascriptNote", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Connection succesful!"))
+  .catch((err) => console.log("erro na conexão= ", err));
