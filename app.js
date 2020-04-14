934c018b93b4be3d2a240aa6ff87b5834b2e2f62
douglasdm <douglasmdamasceno@gmail.com>
2020-04-14 17:18:13 -0300

    add cors na api

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var path = require("path");
 var logger = require("morgan");
 require("./config/database");
+var cors = require("cors");
 var usersRouter = require("./src/routes/users");
 var notesRouter = require("./src/routes/notes");
 
@@ -10,8 +11,8 @@ var app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/users", usersRouter);
 app.use("/notes", notesRouter);
